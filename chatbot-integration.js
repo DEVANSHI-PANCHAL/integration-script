21248f39dd818bd2a8089764f631e34d934349dc
Devanshi Panchal <60812678+DEVANSHI-PANCHAL@users.noreply.github.com>
2024-05-14 14:46:48 +0530

    Update chatbot-integration.js

--- chatbot-integration.js
+++ chatbot-integration.js
@@ -4,7 +4,7 @@
 function loadChatbot() {
     // Create a <script> element to load the chatbot JavaScript
     var scriptElement = document.createElement('script');
-    scriptElement.src = 'https://devanshi-panchal.github.io/chat-plugin/chatbot.js';
+    scriptElement.src = 'https://devanshi-panchal.github.io/chat-plugin/script.js';
 
     // Append the <script> element to the <head> of the target website
     document.head.appendChild(scriptElement);
